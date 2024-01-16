855f18775068954cfadb019f56b5cbd96349d6c0
Fabian <125877740+Fabianphilip@users.noreply.github.com>
2024-01-16 16:55:50 +0100

    0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
 """
-0x03. Minimum Operations - determines minimum number of "copy all"/"paste"
-operations to produce `n` number characters starting from 1.
+minOperations function
 """
 
+
 def minOperations(n):
-    if not isinstance(n, int) or n < 2:
+    """
+    method that calculates the fewest number of operations
+    needed to result in exactly n H characters
+    """
+
+    if type(n) is not int or n < 2:
         return 0
 
-    minOps = 0
+    prime = 0
+    count = 2
 
-    # within the potential factors of n, aside from 1
-    for i in range(2, int(n**0.5) + 1):
-        # for each factor found
-        while n % i == 0:
-            # add that to the total operations
-            minOps += i
-            # refocus to consider the remainder unprinted
-            n //= i
-
-    # If n is still greater than 1, it means n is a prime number
-    if n > 1:
-        minOps += n
-
-    return minOps
+    while count <= n:
+        if n % count == 0:
+            prime += count
+            n = n / count
+        else:
+            count += 1
+    return prime
